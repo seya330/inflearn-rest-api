57876ad7c4687a459d616c054e9714446b3e0701
조세진 <sejin.jo@wadiz.kr>
2021-06-10 11:23:54 +0900

    fail on unknown properties

--- src/test/java/com/honeyshitbug/inflearnrestapi/events/EventControllerTests.java
+++ src/test/java/com/honeyshitbug/inflearnrestapi/events/EventControllerTests.java
@@ -55,12 +55,36 @@ public class EventControllerTests {
         .andExpect(status().isCreated())
         .andDo(print())
         .andExpect(jsonPath("id").exists())
-        .andExpect(jsonPath("id").exists())
         .andExpect(header().exists(HttpHeaders.LOCATION))
         .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
         .andExpect(jsonPath("id").value(Matchers.not(100)))
         .andExpect(jsonPath("free").value(Matchers.not(true)))
         .andExpect(jsonPath("eventStatus").value("DRAFT"))
+    ;
+  }
+
+  @Test
+  void createEvent_badRequest() throws Exception {
+    Event event = Event.builder()
+        .name("Spring")
+        .description("Rest Api Development with Spring")
+        .beginEnrollmentDateTime(LocalDateTime.of(2021, 06, 7, 11, 11))
+        .closeEnrollmentDateTime(LocalDateTime.of(2021, 06, 8, 11, 11))
+        .beginEventDateTime(LocalDateTime.of(2021, 06, 9, 11, 11))
+        .endEventDateTime(LocalDateTime.of(2021, 06, 10, 11, 11))
+        .basePrice(100)
+        .maxPrice(200)
+        .limitOfEnrollment(100)
+        .location("강남역 D2 스타트업 팩토리")
+        .free(true)
+        .build();
+
+    mockMvc.perform(post("/api/events")
+        .contentType(MediaType.APPLICATION_JSON)
+        .accept(MediaTypes.HAL_JSON)
+        .content(objectMapper.writeValueAsString(event)))
+        .andDo(print())
+        .andExpect(status().isBadRequest())
     ;
   }
 }
