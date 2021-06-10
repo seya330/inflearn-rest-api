7961f3300e46185fe7ad255dced55c2ea68c0db3
조세진 <sejin.jo@wadiz.kr>
2021-06-10 17:48:12 +0900

    Add test description annotation

--- src/test/java/com/honeyshitbug/inflearnrestapi/events/EventControllerTests.java
+++ src/test/java/com/honeyshitbug/inflearnrestapi/events/EventControllerTests.java
@@ -28,6 +28,7 @@ public class EventControllerTests {
   ObjectMapper objectMapper;
 
   @Test
+  @TestDescription("정상적으로 이벤트를 생성하는 메소드")
   void createEvent() throws Exception {
     EventDto event = EventDto.builder()
         .name("Spring")
@@ -58,6 +59,7 @@ public class EventControllerTests {
   }
 
   @Test
+  @TestDescription("입력받을 수 없는 값을 사용한 경우에 에러가 발생하는 테스트")
   void createEvent_badRequest() throws Exception {
     Event event = Event.builder()
         .name("Spring")
@@ -83,6 +85,7 @@ public class EventControllerTests {
   }
 
   @Test
+  @TestDescription("입력 값이 비어있는 경우에 에러가 발생하는 테스트")
   void createEvent_badRequest_empty_input() throws Exception {
     EventDto eventDto = EventDto.builder().build();
 
@@ -94,6 +97,7 @@ public class EventControllerTests {
   }
 
   @Test
+  @TestDescription("입력 값이 잘못된 경우에 에러가 발생하는 테스트")
   void createEvent_badRequest_wrong_input() throws Exception{
     EventDto eventDto = EventDto.builder()
         .name("Spring")
