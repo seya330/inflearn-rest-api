d28c2cb28cabc3573037c34284c5bcc752c47dff
조세진 <sejin.jo@wadiz.kr>
2021-06-10 10:53:20 +0900

    통합 테스트 추가

--- src/main/java/com/honeyshitbug/inflearnrestapi/events/EventController.java
+++ src/main/java/com/honeyshitbug/inflearnrestapi/events/EventController.java
@@ -1,6 +1,7 @@
 package com.honeyshitbug.inflearnrestapi.events;
 
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -19,8 +20,12 @@ public class EventController {
 
   private final EventRepository eventRepository;
 
+  private final ModelMapper modelMapper;
+
+
   @PostMapping
-  public ResponseEntity createEvent(@RequestBody Event event) {
+  public ResponseEntity createEvent(@RequestBody EventDto eventDto) {
+    Event event = modelMapper.map(eventDto, Event.class);
     Event newEvent = eventRepository.save(event);
     URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
     return ResponseEntity.created(createdUri).body(event);
--- src/test/java/com/honeyshitbug/inflearnrestapi/events/EventControllerTests.java
+++ src/test/java/com/honeyshitbug/inflearnrestapi/events/EventControllerTests.java
@@ -1,10 +1,13 @@
 package com.honeyshitbug.inflearnrestapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
@@ -17,7 +20,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class EventControllerTests {
 
   @Autowired
@@ -25,9 +29,6 @@ public class EventControllerTests {
 
   @Autowired
   ObjectMapper objectMapper;
-
-  @MockBean
-  EventRepository eventRepository;
 
   @Test
   void createEvent() throws Exception {
@@ -42,9 +43,10 @@ public class EventControllerTests {
         .maxPrice(200)
         .limitOfEnrollment(100)
         .location("강남역 D2 스타트업 팩토리")
+        .eventStatus(EventStatus.PUBLISHED)
+        .free(true)
+        .offline(false)
         .build();
-    event.setId(10);
-    Mockito.when(eventRepository.save(event)).thenReturn(event);
 
     mockMvc.perform(post("/api/events")
         .contentType(MediaType.APPLICATION_JSON)
@@ -55,6 +57,10 @@ public class EventControllerTests {
         .andExpect(jsonPath("id").exists())
         .andExpect(jsonPath("id").exists())
         .andExpect(header().exists(HttpHeaders.LOCATION))
-        .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE));
+        .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+        .andExpect(jsonPath("id").value(Matchers.not(100)))
+        .andExpect(jsonPath("free").value(Matchers.not(true)))
+        .andExpect(jsonPath("eventStatus").value("DRAFT"))
+    ;
   }
 }
