47448a7ace1b44c3658da21ea13338e4fcfadc0e
조세진 <sejin.jo@wadiz.kr>
2021-06-10 17:26:44 +0900

    DTO validation

--- src/main/java/com/honeyshitbug/inflearnrestapi/events/EventController.java
+++ src/main/java/com/honeyshitbug/inflearnrestapi/events/EventController.java
@@ -5,10 +5,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.net.URI;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -24,7 +26,10 @@ public class EventController {
 
 
   @PostMapping
-  public ResponseEntity createEvent(@RequestBody EventDto eventDto) {
+  public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
+    if (errors.hasErrors()) {
+      return ResponseEntity.badRequest().build();
+    }
     Event event = modelMapper.map(eventDto, Event.class);
     Event newEvent = eventRepository.save(event);
     URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
--- src/test/java/com/honeyshitbug/inflearnrestapi/events/EventControllerTests.java
+++ src/test/java/com/honeyshitbug/inflearnrestapi/events/EventControllerTests.java
@@ -3,12 +3,9 @@ package com.honeyshitbug.inflearnrestapi.events;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -32,20 +29,17 @@ public class EventControllerTests {
 
   @Test
   void createEvent() throws Exception {
-    Event event = Event.builder()
+    EventDto event = EventDto.builder()
         .name("Spring")
         .description("Rest Api Development with Spring")
-        .beginEnrollmentDateTime(LocalDateTime.of(2021, 06, 7, 11, 11))
-        .closeEnrollmentDateTime(LocalDateTime.of(2021, 06, 8, 11, 11))
-        .beginEventDateTime(LocalDateTime.of(2021, 06, 9, 11, 11))
-        .endEventDateTime(LocalDateTime.of(2021, 06, 10, 11, 11))
+        .beginEnrollmentDateTime(LocalDateTime.of(2021, 6, 7, 11, 11))
+        .closeEnrollmentDateTime(LocalDateTime.of(2021, 6, 8, 11, 11))
+        .beginEventDateTime(LocalDateTime.of(2021, 6, 9, 11, 11))
+        .endEventDateTime(LocalDateTime.of(2021, 6, 10, 11, 11))
         .basePrice(100)
         .maxPrice(200)
         .limitOfEnrollment(100)
         .location("강남역 D2 스타트업 팩토리")
-        .eventStatus(EventStatus.PUBLISHED)
-        .free(true)
-        .offline(false)
         .build();
 
     mockMvc.perform(post("/api/events")
@@ -68,10 +62,10 @@ public class EventControllerTests {
     Event event = Event.builder()
         .name("Spring")
         .description("Rest Api Development with Spring")
-        .beginEnrollmentDateTime(LocalDateTime.of(2021, 06, 7, 11, 11))
-        .closeEnrollmentDateTime(LocalDateTime.of(2021, 06, 8, 11, 11))
-        .beginEventDateTime(LocalDateTime.of(2021, 06, 9, 11, 11))
-        .endEventDateTime(LocalDateTime.of(2021, 06, 10, 11, 11))
+        .beginEnrollmentDateTime(LocalDateTime.of(2021, 6, 7, 11, 11))
+        .closeEnrollmentDateTime(LocalDateTime.of(2021, 6, 8, 11, 11))
+        .beginEventDateTime(LocalDateTime.of(2021, 6, 9, 11, 11))
+        .endEventDateTime(LocalDateTime.of(2021, 6, 10, 11, 11))
         .basePrice(100)
         .maxPrice(200)
         .limitOfEnrollment(100)
@@ -84,6 +78,17 @@ public class EventControllerTests {
         .accept(MediaTypes.HAL_JSON)
         .content(objectMapper.writeValueAsString(event)))
         .andDo(print())
+        .andExpect(status().isBadRequest())
+    ;
+  }
+
+  @Test
+  void createEvent_badRequest_empty_input() throws Exception {
+    EventDto eventDto = EventDto.builder().build();
+
+    mockMvc.perform(post("/api/events")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(eventDto)))
         .andExpect(status().isBadRequest())
     ;
   }
