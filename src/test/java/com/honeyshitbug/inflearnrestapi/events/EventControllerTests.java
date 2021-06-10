05a9e90afe0fb78bfcc56dee4bb2d11bb60a8cd8
조세진 <sejin.jo@wadiz.kr>
2021-06-10 09:47:59 +0900

    POST /api/events return 201

--- src/test/java/com/honeyshitbug/inflearnrestapi/events/EventControllerTests.java
+++ src/test/java/com/honeyshitbug/inflearnrestapi/events/EventControllerTests.java
@@ -2,8 +2,10 @@ package com.honeyshitbug.inflearnrestapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -24,6 +26,9 @@ public class EventControllerTests {
   @Autowired
   ObjectMapper objectMapper;
 
+  @MockBean
+  EventRepository eventRepository;
+
   @Test
   void createEvent() throws Exception {
     Event event = Event.builder()
@@ -38,6 +43,9 @@ public class EventControllerTests {
         .limitOfEnrollment(100)
         .location("강남역 D2 스타트업 팩토리")
         .build();
+    event.setId(10);
+    Mockito.when(eventRepository.save(event)).thenReturn(event);
+
 
     mockMvc.perform(post("/api/events")
         .contentType(MediaType.APPLICATION_JSON)
