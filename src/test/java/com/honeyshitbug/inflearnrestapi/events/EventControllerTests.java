d64d6fedf144a529dc958d92f24e8cf6e00a100c
조세진 <sejin.jo@wadiz.kr>
2021-06-10 09:51:46 +0900

    polishing

--- src/test/java/com/honeyshitbug/inflearnrestapi/events/EventControllerTests.java
+++ src/test/java/com/honeyshitbug/inflearnrestapi/events/EventControllerTests.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,8 +15,7 @@ import java.time.LocalDateTime;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest
 public class EventControllerTests {
@@ -46,13 +46,15 @@ public class EventControllerTests {
     event.setId(10);
     Mockito.when(eventRepository.save(event)).thenReturn(event);
 
-
     mockMvc.perform(post("/api/events")
         .contentType(MediaType.APPLICATION_JSON)
         .accept(MediaTypes.HAL_JSON)
         .content(objectMapper.writeValueAsString(event)))
         .andExpect(status().isCreated())
         .andDo(print())
-        .andExpect(jsonPath("id").exists());
+        .andExpect(jsonPath("id").exists())
+        .andExpect(jsonPath("id").exists())
+        .andExpect(header().exists(HttpHeaders.LOCATION))
+        .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE));
   }
 }
