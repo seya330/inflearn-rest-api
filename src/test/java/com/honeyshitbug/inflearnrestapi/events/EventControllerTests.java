058db53b7a587ca9479ff406dd76056e7754e4e8
SejinJo <sejinjo@SejinJoui-MacBookAir.local>
2021-06-16 21:49:10 +0900

    test class refactoring

--- src/test/java/com/honeyshitbug/inflearnrestapi/events/EventControllerTests.java
+++ src/test/java/com/honeyshitbug/inflearnrestapi/events/EventControllerTests.java
@@ -1,20 +1,12 @@
 package com.honeyshitbug.inflearnrestapi.events;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.honeyshitbug.inflearnrestapi.common.RestDocsConfiguration;
+import com.honeyshitbug.inflearnrestapi.common.BaseControllerTest;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 import java.util.stream.IntStream;
@@ -27,24 +19,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs
-@Import(RestDocsConfiguration.class)
-@ActiveProfiles("test")
-class EventControllerTests {
-
-  @Autowired
-  MockMvc mockMvc;
-
-  @Autowired
-  ObjectMapper objectMapper;
+class EventControllerTests extends BaseControllerTest {
 
   @Autowired
   EventRepository eventRepository;
-
-  @Autowired
-  ModelMapper modelMapper;
 
   @Test
   @TestDescription("정상적으로 이벤트를 생성하는 메소드")
--- src/test/java/com/honeyshitbug/inflearnrestapi/index/IndexControllerTest.java
+++ src/test/java/com/honeyshitbug/inflearnrestapi/index/IndexControllerTest.java
@@ -1,28 +1,13 @@
 package com.honeyshitbug.inflearnrestapi.index;
 
-import com.honeyshitbug.inflearnrestapi.common.RestDocsConfiguration;
+import com.honeyshitbug.inflearnrestapi.common.BaseControllerTest;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs
-@Import(RestDocsConfiguration.class)
-@ActiveProfiles("test")
-class IndexControllerTest {
-
-  @Autowired
-  MockMvc mockMvc;
+class IndexControllerTest extends BaseControllerTest {
 
   @Test
   void index() throws Exception {
